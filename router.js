072bf2c8781d45475be51945424ed72d37456cf5
Manel Pavon <manelpavon10@gmail.com>
2016-11-18 17:17:56 +0100

    add routes api

--- router.js
+++ router.js
@@ -4,11 +4,22 @@ const router = require('koa-router')();
 const bodyParser = require('body-parser');
 
 const usersCtrl   = require('./controllers/users.js');
+const recipesCtrl = require('./controllers/recipes.js');
+const categoriesCtrl = require('./controllers/categories.js');
 
 const authMiddleware = require('./auth.js')
 
 router.get('/login', usersCtrl.login);
 router.post('/login', usersCtrl.createUser);
+
+router.get('/recipes', recipesCtrl.getRecipes);
+router.get('/recipes/:id', recipesCtrl.getSpecificRecipe);
+router.post('/recipes', recipesCtrl.postRecipe);
+
+router.get('/categories', categoriesCtrl.getCategories);
+router.get('/categories/:id', categoriesCtrl.getSpecificCategory);
+
+
 
 // router.get('/panels', authMiddleware, panelsCtrl.getPanels);
 // router.post('/panels', authMiddleware, panelsCtrl.createMockPanel);
